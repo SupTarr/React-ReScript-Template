c90107f1308a69e28ff1250070752a8e9c00a069
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-12-07 15:40:14 +0700

    Add introduction page

--- src/App.bs.js
+++ src/App.bs.js
@@ -2,8 +2,31 @@
 
 import * as React from "react";
 
+import './App.css'
+;
+
 function App(Props) {
-  return React.createElement(React.Fragment, undefined, React.createElement("div", undefined, React.createElement("h1", undefined, "Hello, world!"), React.createElement("p", undefined, "This is a paragraph")));
+  return React.createElement("div", {
+              className: "App"
+            }, React.createElement("div", undefined, React.createElement("a", {
+                      href: "https://vitejs.dev",
+                      target: "_blank"
+                    }, React.createElement("img", {
+                          className: "logo",
+                          alt: "Vite logo",
+                          src: "/vite.svg"
+                        })), React.createElement("a", {
+                      href: "https://reactjs.org",
+                      target: "_blank"
+                    }, React.createElement("img", {
+                          className: "logo react",
+                          alt: "React logo",
+                          src: "./assets/react.svg"
+                        }))), React.createElement("h1", undefined, "Vite + React"), React.createElement("div", {
+                  className: "card"
+                }, React.createElement("button", undefined, "count is"), React.createElement("p", undefined, "Edit", React.createElement("code", undefined, "src/App.res"), "and save to test HMR")), React.createElement("p", {
+                  className: "read-the-docs"
+                }, "Click on the Vite and React logos to learn more"));
 }
 
 var make = App;
@@ -11,4 +34,4 @@ var make = App;
 export {
   make ,
 }
-/* react Not a pure module */
+/*  Not a pure module */
--- src/App.res
+++ src/App.res
@@ -1,11 +1,28 @@
 // App.res
+%%raw("import './App.css'")
 
 @react.component
 let make = () => {
-  <>
+  <div className="App">
     <div>
-      <h1> {React.string("Hello, world!")} </h1>
-      <p> {React.string("This is a paragraph")} </p>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank">
+        <img src="./assets/react.svg" className="logo react" alt="React logo" />
+      </a>
     </div>
-  </>
+    <h1> {React.string("Vite + React")} </h1>
+    <div className="card">
+      <button> {React.string("count is")} </button>
+      <p>
+        {React.string("Edit")}
+        <code> {React.string("src/App.res")} </code>
+        {React.string("and save to test HMR")}
+      </p>
+    </div>
+    <p className="read-the-docs">
+      {React.string("Click on the Vite and React logos to learn more")}
+    </p>
+  </div>
 }
